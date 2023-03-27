4a109be89a97756fcc3444692a090901744bd5a6
VVebber <malihevdanil@yandex.ru>
2023-03-28 01:27:02 +0300

    version 4.4.4

---
+++ Javascript/console.js
@@ -0,0 +1,15 @@
+let integer = document.getElementById("integer28");
+for( let i=2; i<=28; i++){
+    const p = document.createElement("p");
+    p.className="null";
+    p.innerText = i;
+    integer.appendChild(p);
+}
+
+let integer2 = document.getElementById("integer9");
+for( let i=2; i<=15; i++){
+    const p = document.createElement("p");
+    p.className="null";
+    p.innerText = i;
+    integer2.appendChild(p);
+}
