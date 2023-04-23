71713ab9c485bfc24ad20bcd80c1a960b05f9fa4
VVebber <malihevdanil@yandex.ru>
2023-04-23 20:15:19 +0300

    gh

--- Javascript/console.js
+++ Javascript/console.js
@@ -9,12 +9,16 @@ function createNumberSequence(start, end, containerId) {
     }
 }
 
+createNumberSequence(2, 31, "integer31");
 createNumberSequence(2, 28, "integer28");
 createNumberSequence(2, 25, "integer25");
 createNumberSequence(2, 24, "integer24");
 createNumberSequence(2, 23, "integer23");
 createNumberSequence(2, 22, "integer22");
+createNumberSequence(2, 21, "integer21");
 createNumberSequence(2, 20, "integer20");
+createNumberSequence(2, 19, "integer19");
+createNumberSequence(2, 18, "integer18");
 createNumberSequence(2, 17, "integer17");
 createNumberSequence(2, 16, "integer16");
 createNumberSequence(2, 15, "integer15");
@@ -23,12 +27,18 @@ createNumberSequence(2, 13, "integer13");
 createNumberSequence(2, 12, "integer12");
 createNumberSequence(2, 11, "integer11");
 createNumberSequence(2, 9, "integer9");
-createNumberSequence(2, 8, "integer8");
-createNumberSequence(2, 7, "integer7");
-createNumberSequence(2, 6, "integer6");
+ createNumberSequence(2, 6, "integer6");
 const containers10 = ["integer10", "integer10_2", "integer10_3", "integer10_4", "integer10_5", "integer10_6"];
 
 containers10.forEach(container => createNumberSequence(2, 10, container));
+
+const containers8 = ["integer8", "integer8_2"];
+
+containers8.forEach(container => createNumberSequence(2, 8, container));
+
+const containers7 = ["integer7", "integer7_2"];
+
+containers7.forEach(container => createNumberSequence(2, 7, container));
 
 const containers5 = ["integer5", "integer5_2", "integer5_3", "integer5_4", "integer5_5", "integer5_6"];
 
